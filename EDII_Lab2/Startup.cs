b918af156bde2730f0d612db4a0377ffec81dc00
KEVIN ORTIZ RAMIREZ <kortiz@correo.url.edu.gt>
2020-02-13 11:42:18 -0600

    Corrigiendo startup

--- EDII_Lab2/Startup.cs
+++ EDII_Lab2/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using EDII_Lab2.Models;
 
 namespace EDII_Lab2
 {
@@ -26,6 +27,7 @@ namespace EDII_Lab2
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+			services.AddSingleton<Soda>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
