a804c5f5b13d70c8fd256762b6ff8122cf73a79a
KEVIN ORTIZ RAMIREZ <kortiz@correo.url.edu.gt>
2020-02-21 20:10:52 -0600

    Escritura en archivo + nodo de archivo

---
+++ EDII_Lab2/Arbol/ArbolB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDII_Lab2.Arbol
+{
+    public class ArbolB
+    {
+        //Agregar
+        //Escribir
+        //Modificar
+        //Eliminar
+    }
+}
